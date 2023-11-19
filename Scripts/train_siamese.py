283974c136440618ac2734c03b40f1c087a803ab
Nikolai <79812662+nikolaibeck@users.noreply.github.com>
2023-11-19 15:34:30 +0100

    Update train_siamese.py

--- Scripts/train_siamese.py
+++ Scripts/train_siamese.py
@@ -29,7 +29,7 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
 parser = argparse.ArgumentParser()
 parser.add_argument("--train_batch_size", default=64, type=int)
 parser.add_argument("--max_seq_length", default=300, type=int)
-parser.add_argument("--model_name", required=True)
+parser.add_argument("--model_name", default='distilbert-base-uncased')
 parser.add_argument("--max_passages", default=0, type=int)
 parser.add_argument("--epochs", default=10, type=int)
 parser.add_argument("--pooling", default="mean")
@@ -132,95 +132,97 @@ logging.info("Load CrossEncoder scores dict")
 with gzip.open(ce_scores_file, 'rb') as fIn:
     ce_scores = pickle.load(fIn)
 
-# As training data we use hard-negatives that have been mined using various systems
-hard_negatives_filepath = os.path.join(data_folder, 'msmarco-hard-negatives.jsonl.gz')
-if not os.path.exists(hard_negatives_filepath):
-    logging.info("Download cross-encoder scores file")
-    util.http_get('https://huggingface.co/datasets/sentence-transformers/msmarco-hard-negatives/resolve/main/msmarco-hard-negatives.jsonl.gz', hard_negatives_filepath)
+# # As training data we use hard-negatives that have been mined using various systems
+# hard_negatives_filepath = os.path.join(data_folder, 'msmarco-hard-negatives.jsonl.gz')
+# if not os.path.exists(hard_negatives_filepath):
+#     logging.info("Download cross-encoder scores file")
+#     util.http_get('https://huggingface.co/datasets/sentence-transformers/msmarco-hard-negatives/resolve/main/msmarco-hard-negatives.jsonl.gz', hard_negatives_filepath)
 
 
-logging.info("Read hard negatives train file")
-train_queries = {}
-negs_to_use = None
-with gzip.open(hard_negatives_filepath, 'rt') as fIn:
-    for line in tqdm.tqdm(fIn):
-        if max_passages > 0 and len(train_queries) >= max_passages:
-            break
-        data = json.loads(line)
+#logging.info("Read hard negatives train file")
+# train_queries = {}
+# negs_to_use = None
+# with gzip.open(hard_negatives_filepath, 'rt') as fIn:
+#     for line in tqdm.tqdm(fIn):
+#         if max_passages > 0 and len(train_queries) >= max_passages:
+#             break
+#         data = json.loads(line)
 
-        #Get the positive passage ids
-        pos_pids = data['pos']
+#         #Get the positive passage ids
+#         pos_pids = data['pos']
 
-        #Get the hard negatives
-        neg_pids = set()
-        if negs_to_use is None:
-            if args.negs_to_use is not None:    #Use specific system for negatives
-                negs_to_use = args.negs_to_use.split(",")
-            else:   #Use all systems
-                negs_to_use = list(data['neg'].keys())
-            logging.info("Using negatives from the following systems:", negs_to_use)
+#         #Get the hard negatives
+#         neg_pids = set()
+#         if negs_to_use is None:
+#             if args.negs_to_use is not None:    #Use specific system for negatives
+#                 negs_to_use = args.negs_to_use.split(",")
+#             else:   #Use all systems
+#                 negs_to_use = list(data['neg'].keys())
+#             logging.info("Using negatives from the following systems:", negs_to_use)
 
-        for system_name in negs_to_use:
-            if system_name not in data['neg']:
-                continue
+#         for system_name in negs_to_use:
+#             if system_name not in data['neg']:
+#                 continue
 
-            system_negs = data['neg'][system_name]
-            negs_added = 0
-            for pid in system_negs:
-                if pid not in neg_pids:
-                    neg_pids.add(pid)
-                    negs_added += 1
-                    if negs_added >= num_negs_per_system:
-                        break
+#             system_negs = data['neg'][system_name]
+#             negs_added = 0
+#             for pid in system_negs:
+#                 if pid not in neg_pids:
+#                     neg_pids.add(pid)
+#                     negs_added += 1
+#                     if negs_added >= num_negs_per_system:
+#                         break
 
-        if args.use_all_queries or (len(pos_pids) > 0 and len(neg_pids) > 0):
-            train_queries[data['qid']] = {'qid': data['qid'], 'query': queries[data['qid']], 'pos': pos_pids, 'neg': neg_pids}
+#         if args.use_all_queries or (len(pos_pids) > 0 and len(neg_pids) > 0):
+#             train_queries[data['qid']] = {'qid': data['qid'], 'query': queries[data['qid']], 'pos': pos_pids, 'neg': neg_pids}
 
-logging.info("Train queries: {}".format(len(train_queries)))
+# logging.info("Train queries: {}".format(len(train_queries)))
 
 # We create a custom MSMARCO dataset that returns triplets (query, positive, negative)
 # on-the-fly based on the information from the mined-hard-negatives jsonl file.
 class MSMARCODataset(Dataset):
-    def __init__(self, queries, corpus, ce_scores):
-        self.queries = queries
-        self.queries_ids = list(queries.keys())
-        self.corpus = corpus
-        self.ce_scores = ce_scores
+    def __init__(self, query_corpus, passage_corpus, ce_scores):
+        
+        logging.info("Loading data")
+        query_ids = []
+        passage_ids = []
+        ce_scores_list = []
+        
+        for i, qid in enumerate(ce_scores.keys()):
+            if i > 5000:
+                continue
+            passage_dict = ce_scores[qid]
+            query_ids += [qid] * len(passage_dict)
+            for pid in passage_dict:
+                passage_ids.append(pid)
+                ce_scores_list.append(passage_dict[pid])
 
-        for qid in self.queries:
-            self.queries[qid]['pos'] = list(self.queries[qid]['pos'])
-            self.queries[qid]['neg'] = list(self.queries[qid]['neg'])
-            random.shuffle(self.queries[qid]['neg'])
+        del ce_scores
+
+        self.queries = query_corpus
+        self.queries_ids = query_ids
+        self.passage_ids = passage_ids
+        self.corpus = corpus
+        self.ce_scores = ce_scores_list
 
     def __getitem__(self, item):
-        query = self.queries[self.queries_ids[item]]
-        query_text = query['query']
-        qid = query['qid']
 
-        if len(query['pos']) > 0:
-            pos_id = query['pos'].pop(0)    #Pop positive and add at end
-            pos_text = self.corpus[pos_id]
-            query['pos'].append(pos_id)
-        else:   #We only have negatives, use two negs
-            pos_id = query['neg'].pop(0)    #Pop negative and add at end
-            pos_text = self.corpus[pos_id]
-            query['neg'].append(pos_id)
+        # fetch relevant items
+        passage_id = self.passage_ids[item]
+        query_id = self.queries_ids[item]
+        score = self.ce_scores[item]
+        
+        # extract text of passage and queries
+        query_text = self.queries[query_id]
+        passage_text = self.corpus[passage_id]
 
-        #Get a negative passage
-        neg_id = query['neg'].pop(0)    #Pop negative and add at end
-        neg_text = self.corpus[neg_id]
-        query['neg'].append(neg_id)
-
-        pos_score = self.ce_scores[qid][pos_id]
-        neg_score = self.ce_scores[qid][neg_id]
-
-        return InputExample(texts=[query_text, pos_text, neg_text], label=pos_score)
+        return InputExample(texts=[query_text, passage_text], label=score)
 
     def __len__(self):
-        return len(self.queries)
+        return len(self.passage_ids)
 
 # For training the SentenceTransformer model, we need a dataset, a dataloader, and a loss used for training.
-train_dataset = MSMARCODataset(queries=train_queries, corpus=corpus, ce_scores=ce_scores)
+train_dataset = MSMARCODataset(query_corpus=queries, passage_corpus=corpus, ce_scores=ce_scores)
 train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=train_batch_size, drop_last=True)
 train_loss = CE_MSELoss(model=model)
 
@@ -233,6 +235,9 @@ model.fit(train_objectives=[(train_dataloader, train_loss)],
           checkpoint_save_steps=10000,
           optimizer_params = {'lr': args.lr},
           )
+
+# Train latest model
+model.save(model_save_path)
 
 # Train latest model
 model.save(model_save_path)
