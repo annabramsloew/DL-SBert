3fbbab31e5cdef3a4a52ab8e6f79438f09d9eb7f
annabramsloew <80269825+annabramsloew@users.noreply.github.com>
2023-12-20 18:25:09 +0100

    Update InputExample.py

--- Scripts/sentence_transformer_dtu/readers/InputExample.py
+++ Scripts/sentence_transformer_dtu/readers/InputExample.py
@@ -1,3 +1,12 @@
+"""
+The structure of our data pipeline and modelling approach has been based off of
+the work by UKPLab based in Germany. Their repository may be found here: 
+https://github.com/UKPLab/sentence-transformers. We have adapted code from
+this repository to be used for our project.
+
+This script was created by UKPLab.
+"""
+
 from typing import Union, List
 
 
